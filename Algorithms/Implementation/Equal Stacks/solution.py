6c1ba322989cab6cccb4ef7abc0b6d6fcca0020b
basanthjenuhb <basanthjenuhb@gmail.com>
2016-08-12 23:20:27 +0530

    Solved Implementation/Equal Stacks

--- Algorithms/Implementation/Equal Stacks/solution.py
+++ Algorithms/Implementation/Equal Stacks/solution.py
@@ -4,13 +4,7 @@ h2 = map(int,raw_input().split())
 h3 = map(int,raw_input().split())
 s1,s2,s3 = sum(h1),sum(h2),sum(h3)
 while not (s1==s2 and s1==s3):
-    if s1 >= s2 and s1>=s3:
-        s1-=h1[0]
-        h1.pop(0)
-    elif s2 >= s1 and s2>=s3:
-        s2-=h2[0]
-        h2.pop(0)
-    elif s3 >= s1 and s3 >= s2:
-        s3-=h3[0]
-        h3.pop(0)
+    if s1 >= s2 and s1>=s3: s1 -= h1.pop(0)
+    elif s2 >= s1 and s2>=s3: s2 -= h2.pop(0)
+    elif s3 >= s1 and s3 >= s2: s3 -= h3.pop(0)
 print s1
